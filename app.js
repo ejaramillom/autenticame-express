d03595e05df611d0909909d8398fa624b3819fd1
ejaramillom <ejaramilo@hotmail.com>
2020-04-03 17:21:24 -0500

    changed database

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
-const express = require( "express" );
-const mongoose = require( "mongoose" );
-const path = require( "path" );
-const routes = require( "./routes" );
-const cookieParser = require( "cookie-parser" );
-const cookieSession = require( "cookie-session")
-const db = mongoose.connection;
+const express = require("express");
+const cookieSession = require('cookie-session')
+const mongoose = require("mongoose");
+const path = require('path');
+require("./user");
+const routes = require('./routes');
+
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/users", { useNewUrlParser: true });
+
 const app = express();
-require( "./user" );
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieSession({ secret: "session" }));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/', routes);
 
-mongoose.connect( process.env.MONGODB_URL || "mongodb://localhost:27017/userOne", { useNewUrlParser: true });
-mongoose.connection.on( "error", function( e ) { console.error( e ); });
-
-app.set( "view engine", "pug" );
-app.set( "views", "views" );
-app.use( cookieSession({ secret: "session" }));
-app.use( express.urlencoded({ extended: true }));
-app.use( cookieParser() );
-app.use( "/", routes);
-app.use( "/public", express.static(process.cwd() + "/public"));
-
-app.listen( 3000, () => console.log( "Listening on port 3000 ..." ));
+app.listen(3000, () => console.log("Listening on port 3000 ..."));
